bf5d3122f411c93c6c9c2990f5358d57f9b08396
nico <Nicolas.FRANCO@etu.uca.fr>
2023-12-02 18:40:26 +0100

    Exchange working ! :tada:

--- DevGardenAPI/Controllers/OAuthController.cs
+++ DevGardenAPI/Controllers/OAuthController.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 namespace DevGardenAPI.Controllers
 {
     /*
-     * 
+     * Controller that handles the OAuth flow.
+     * POC for now, maybe a more concrete version needs to be implemented.
      */
-    [Route("api/[controller]")]
+    [Route("api/oauth")]
     [ApiController]
     public class OAuthController : ControllerBase
     {
@@ -25,11 +26,13 @@ namespace DevGardenAPI.Controllers
         public async Task<IActionResult> ExchangeToken([FromBody] TokenRequest request)
         {
             _logger.LogInformation($"Received request with code: {request?.Code}");
+            Console.WriteLine($"Received request with code: {request?.Code}");
 
             try
             {
                 var requestBody = new FormUrlEncodedContent(new[]
                 {
+                    // [TODO] Move this to a config file in a safe way .
                     new KeyValuePair<string, string>("client_id", "e2ab8ffbefc5b983f71b"),
                     new KeyValuePair<string, string>("client_secret", "85fa33682f94f8f98fea2c03b499f403c6ed90b4"),
                     new KeyValuePair<string, string>("code", request.Code),
@@ -43,10 +46,25 @@ namespace DevGardenAPI.Controllers
                     response.EnsureSuccessStatusCode();
 
                     var responseContent = await response.Content.ReadAsStringAsync();
-                    var responseData = JsonConvert.DeserializeObject<TokenResponse>(responseContent);
-                    _logger.LogInformation($"Access token received: {responseData?.AccessToken}");
+                    // Read the response RAW
+                    _logger.LogInformation($"Raw Response Content: {responseContent}");
 
-                    return Ok(responseData);
+
+                    var accessToken = ExtractAccessToken(responseContent);
+
+                    // Error handling
+                    if (!string.IsNullOrEmpty(accessToken))
+                    {
+                        _logger.LogInformation($"Access token received: {accessToken}");
+
+                        // Return an OK response with the access token as JSON object
+                        return Ok(new { access_token = accessToken });
+                    }
+                    else
+                    {
+                        _logger.LogError("Failed to extract access token from response.");
+                        return StatusCode(500, new { error = "Internal Server Error" });
+                    }
                 }
             }
             catch (Exception ex)
@@ -54,6 +72,20 @@ namespace DevGardenAPI.Controllers
                 _logger.LogError($"Error in token exchange: {ex.Message}");
                 return StatusCode(500, "Internal Server Error");
             }
+        }
+
+        /*
+         * Extracts the access token from the response content.
+         * I made this based on the raw format when testing with GitHub API.
+         * [TODO] Maybe a different version depending on the service (Gitea, Gitlab...).
+         */
+        private string ExtractAccessToken(string responseContent)
+        {
+            // Gets the value of a query string parameter from the response content.
+            // example: access_token=123456789&scope=repo%2Cgist&token_type=bearer
+            // queryString["access_token"] will return 123456789
+            var queryString = System.Web.HttpUtility.ParseQueryString(responseContent);
+            return queryString["access_token"];
         }
     }
 }
--- DevGardenAPI/Program.cs
+++ DevGardenAPI/Program.cs
@@ -1,8 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddHttpClient();
+
+// CORS configuration
+// To allow our React Client to access our API
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactNativeApp", builder =>
+    {
+        builder.WithOrigins("http://localhost:19006")
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
+// Add logging
+builder.Logging.AddConsole();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,6 +35,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// Enable CORS
+app.UseCors("AllowReactNativeApp");
 
 app.MapControllers();
 
