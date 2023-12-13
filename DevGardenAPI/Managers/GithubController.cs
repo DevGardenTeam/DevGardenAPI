71ecde3e7e48cd19c2749d32edf2711ec03e92bc
lobroda <lou.broda@etu.uca.fr>
2023-12-13 23:01:20 +0100

    ADD : Avancement général + first mock method (not working on Swagger)

--- DevGardenAPI/Managers/GithubController.cs
+++ DevGardenAPI/Managers/GithubController.cs
@@ -1,8 +1,14 @@
 ﻿using DevGardenAPI.GenericRepository;
+using Microsoft.AspNetCore.Mvc;
 using Model;
+using Newtonsoft.Json.Linq;
+using System.Net;
 
 namespace DevGardenAPI.Managers
-{
+{ 
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/git/[controller]")]
     public class GithubController<T> : DevGardenController<T> where T : ModelBase
     {
         #region Properties
@@ -10,7 +16,7 @@ namespace DevGardenAPI.Managers
         #endregion
 
         #region Constructor
-
+        
         public GithubController(IRepository<T> repository) : base(repository)
         {
         }
@@ -18,6 +24,38 @@ namespace DevGardenAPI.Managers
         #endregion
 
         #region Methods
+
+        [ApiVersion("1.0")]
+        [HttpGet]
+        public async Task<HttpResponseMessage> Get()
+        {
+            Logger.Debug($"{nameof(GithubController<T>)} - {nameof(Get)} - Starting");
+
+            try
+            {
+                string username = "LouBRODA";
+                string token = "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";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("User-Agent", "DevGarden");
+                    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+                    string apiUrl = $"https://api.github.com/users/{username}/repos";
+
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    return response;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"{nameof(GithubController<T>)} - {nameof(Get)} - Error");
+                Logger.Error($"{nameof(Get)} - {ex.InnerException}");
+
+                return null;//Request.CreateResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
 
         #endregion
     }
