83818422f4fce38acc5ef07a5f4a7e2db531cd98
nico-dev <nico-dev@T480S>
2023-12-31 18:05:33 +0100

    started extending oAuth handler structure to integrate other platforms (Gitlab, Gitea...)

---
+++ Auth/TokenRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth
+{
+    /*
+     * This class is used to deserialize the response from the GitHub API.
+     */
+    public class TokenRequest
+    {
+        public string Code { get; set; }
+    }
+}
