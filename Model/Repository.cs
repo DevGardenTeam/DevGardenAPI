a5af9cad063b58f5c65db6b6023ba27efae85405
lobroda <lou.broda@etu.uca.fr>
2024-01-12 14:54:12 +0100

    ADD : Début implémentation dynamique JsonProperty (not working - exception)

--- Model/Repository.cs
+++ Model/Repository.cs
@@ -18,12 +18,13 @@ namespace Model
         private string _description;
         private bool _isFork;
         private string _url;
+        private DateTime _creationDate;
+        private string _language;
+        private long _size;
         private List<Branch> _branches = new();
         private List<Commit> _commits = new();
         private List<Member> _contributors = new();
         private List<Issue> _issues = new();
-        private string _language;
-        private long _size;
         private List<Folder>? _folders = new();
         private List<File> _files = new();
 
@@ -31,13 +32,13 @@ namespace Model
 
         #region Properties
 
-        [JsonProperty("name")]
+        [JsonProperty(nameof(JsonPropertyName))]
         public string Name { get; set; }
 
         [JsonProperty("owner")]
         public Member Owner { get; set; }
 
-        [JsonProperty("private")]
+        [JsonProperty(nameof(JsonPropertyIsPrivate))]
         public bool IsPrivate { get; set; }
 
         [JsonProperty("description")]
@@ -50,7 +51,7 @@ namespace Model
         public string Url { get; set; }
 
         [JsonProperty("created_at")]
-        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public DateTime CreationDate { get; set; }
 
         [JsonProperty("language")]
         public string Language { get; set; }
@@ -68,7 +69,17 @@ namespace Model
 
         public ReadOnlyCollection<Folder>? Folders { get; set; }
 
-        public ReadOnlyCollection<File> Files { get; set; }        
+        public ReadOnlyCollection<File> Files { get; set; }
+
+        public string JsonPropertyName { get; set; }
+        public string JsonPropertyOwner { get; set; }
+        public string JsonPropertyIsPrivate { get; set; }
+        public string JsonPropertyDescription { get; set; }
+        public string JsonPropertyIsFork { get; set; }
+        public string JsonPropertyUrl { get; set; }
+        public string JsonPropertyCreationDate { get; set; }
+        public string JsonPropertyLanguage { get; set; }
+        public string JsonPropertySize { get; set; }
 
         #endregion
 
@@ -76,17 +87,20 @@ namespace Model
 
         public Repository()
         {
+            InitRepositoryDynamicPropertyJson();
             Name = _name;
             Owner = _owner;
             IsPrivate = _isPrivate;
             Description = _description;
             IsFork = _isFork;
             Url = _url;
+            CreationDate = _creationDate;
+            Language = _language;
+            Size = _size;
             Branches = new ReadOnlyCollection<Branch>(_branches);
             Commits = new ReadOnlyCollection<Commit>(_commits);
             Contributors = new ReadOnlyCollection<Member>(_contributors);
             Issues = new ReadOnlyCollection<Issue>(_issues);
-            Language = _language;
             Folders = new ReadOnlyCollection<Folder>(_folders);
             Files = new ReadOnlyCollection<File>(_files);
         }
@@ -94,6 +108,19 @@ namespace Model
         #endregion
 
         #region Methods
+
+        public void InitRepositoryDynamicPropertyJson()
+        {
+            JsonPropertyName = "name";
+            JsonPropertyOwner = "owner";
+            JsonPropertyIsPrivate = "private";
+            JsonPropertyDescription = "description";
+            JsonPropertyIsFork = "fork";
+            JsonPropertyUrl = "url";
+            JsonPropertyCreationDate = "created_at";
+            JsonPropertyLanguage = "language";
+            JsonPropertySize = "size";
+        }
 
         public bool Equals(Repository? other)
         {
