822abe1a58d814d57c59434e05f5137bad87a0e6
Nico <nicolas.franco@etu.uca.fr>
2024-02-06 13:05:32 +0100

    :rocket: slight swagger config change

--- DevGardenAPI/Program.cs
+++ DevGardenAPI/Program.cs
@@ -71,7 +71,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => 
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "DevGardenAPI v1");
+    }
+    );
 }
 
 app.UseHttpsRedirection();
