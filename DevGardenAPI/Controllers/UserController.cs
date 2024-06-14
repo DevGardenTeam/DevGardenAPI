e622601362e39af188db3c6f74ebfd86127bea5c
tilevadoux <tim.levadoux@etu.uca.fr>
2024-06-14 15:36:01 +0200

    test

--- DevGardenAPI/Controllers/UserController.cs
+++ DevGardenAPI/Controllers/UserController.cs
@@ -66,7 +66,7 @@ namespace DevGardenAPI.Controllers
                 ServiceName = servicename
             };
 
-            // if (!await userController.UserExists(username)){
+            // if (!userController.UserExists(username)){
             //     return Conflict("Erreur : le user n'existe pas");
             // }
 
